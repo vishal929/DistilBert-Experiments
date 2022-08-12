23b17cb8179ef2646c374dd35513eadeb605fa20
vishal929 <48373882+vishal929@users.noreply.github.com>
2022-08-12 15:33:37 -0400

    have some code for value store, but will be tough to think about how to approach in multi-headed way. Will revisit after writing code for testing suite

--- Modules/ValueStore.py
+++ Modules/ValueStore.py
@@ -4,7 +4,13 @@
 
 import torch
 import faiss
+import os
+import numpy as np
 
+# external file for indexing that needs to spill to disk
+db_file_path = os.path.join(os.getcwd(),"Resources","value_store")
+
+# this represents the gating module that "chooses" between model values and memory store values
 class GatedLocalValueAttention(torch.nn.Module):
     def __init__(self, in_dim):
         super().__init__()
@@ -29,26 +35,61 @@ class KeyValueStoreLayer(torch.nn.Module):
     """
     def __init__(self, memory_size, top_k, in_dim, out_dim):
         super().__init__()
-        # initializing the key value store
-        self.memory =
+        # initializing the key value store -> in_dim *2 for memory dimension since we are storing key,value pairs
+        self.memory = MemoryStore(memory_size,top_k,in_dim*2)
         self.localAttention = torch.nn.MultiheadAttention()
         self.memoryAttention = torch.nn.MultiheadAttention()
-        self.gate = GatedLocalValueAttention()
+        self.gate = GatedLocalValueAttention(out_dim)
 
     def forward(self, query):
-        # computing local attention (self attention in this case)
-        locAttention = self.localAttention.forward(query,query,query)
-        # using knn on key value store, computing attention, and then updating the store
+        # getting the key values from knn
+        # if memory store is on cpu, we can do this asynchronously while gpu does some work
+        # using knn on key value store
         memKeys = None
         memVals = None
+
+        # computing local attention (self attention in this case)
+        locAttention = self.localAttention.forward(query,query,query)
+
         memAttention = self.memoryAttention.forward()
 
         finalVal = self.gate(locAttention, memAttention)
 
         # updating the key value store to include the new key value pairs
+        # we can asynchronously add to continue with the model on gpu if memory store is on the cpu
+
+
 
         return finalVal
 
-    def clearStore(self):
-        # clearing memory (i.e when training on a new document)
-        pass
+
+# defining memory here for faiss
+class MemoryStore():
+    def __init__(self, memory_size, top_k, dimension):
+        self.db = faiss.IndexHNSWFlat(dimension)
+        self.memory_size = memory_size
+        self.top_k = top_k
+        self.dimension = dimension
+
+    def use_gpu(self):
+        self.db = faiss.GpuIndexIVFPQ(self.dimension)
+
+    def use_cpu(self):
+        self.db = faiss.IndexIVFPQ(self.dimension)
+
+    def clear(self):
+        self.db.reset()
+
+    # given a batch of vectors to add to the memory store, we may need to remove older vectors
+    # we are not using add_with_ids, so faiss will give sequential ids for adds -> we can use this fact for removal
+    def add(self, batch):
+        if batch.shape[0] + self.db.ntotal > self.memory_size:
+            # we need to remove some old vectors from the memory store
+            num_to_remove = batch.shape[0] + self.db.ntotal - self.memory_size
+            # removing from beginning of index
+            self.db.remove_ids(np.arange(num_to_remove))
+        self.db.add(batch)
+
+    # performs the knn search using a query batch
+    def knn_search(self, query):
+        return self.db.search(query,self.top_k)
