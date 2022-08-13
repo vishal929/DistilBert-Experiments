f5e91ac7b581c4715149d7388b760f929f4829b4
vishal929 <48373882+vishal929@users.noreply.github.com>
2022-08-13 12:36:24 -0400

    start logic for generic evaluation using huggingface datasets import

---
+++ test.py
@@ -0,0 +1,25 @@
+# status bar
+import torch
+from tqdm.auto import tqdm
+# evaluation logic
+
+# function for evaluation on Squad v1.1 dataset
+def evaluateSquad(model, batch_size):
+    # grabbing squad evaluation set
+    pass
+
+# evaluating openbook QA
+def evaluateOpenbookQA(model, batch_size):
+    pass
+
+# evaluating on medQA-USMLE
+def evaluateMedQA(model, batch_size):
+    pass
+
+#evaluating on commonsense QA
+def evaluateCommonsenseQA(model, batch_size):
+    pass
+
+# evaluating on PG-19 language modeling
+def evaluatePG19(model, batch_size):
+    pass
